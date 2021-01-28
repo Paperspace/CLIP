a848eed2b66445ee3668b6776f3059ffaea5cb85
Dillon Erb <585865+dte@users.noreply.github.com>
2021-01-28 16:13:44 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import clip
 from PIL import Image
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, transform = clip.load("/inputs/pretrainedModel/ViT-B-32", device=device)
+# model, transform = clip.load("/inputs/pretrainedModel/ViT-B-32", device=device)
+model, preprocess = clip.load("ViT-B/32", device=device)
 
 image = transform(Image.open("CLIP.png")).unsqueeze(0).to(device)
 text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
